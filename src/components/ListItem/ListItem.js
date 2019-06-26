f97dd55af4e79065552aed260ad5e21b9885a09f
Mary Kate <MaryKate@Marys-MBP-3.lan1>
2019-06-26 10:45:49 -0400

    local image test

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
 
 const listItem = (props) => (
     <TouchableOpacity onPress={props.onItemPressed}>
-        <View style={styles.listItem} > 
+        <View style={styles.listItem}>
+            <Image resizeMode="cover" source={props.placeImage} style={styles.placeImage} /> 
             <Text>{props.placeName}</Text>
         </View>
     </TouchableOpacity>
@@ -15,7 +16,15 @@ const styles = StyleSheet.create({
         width: "100%",
         padding: 10,
         marginBottom: 5,
-        backgroundColor: "#eee"
+        backgroundColor: "#eee",
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    placeImage: {
+        marginRight: 8,
+        height: 30,
+        width: 30
     }
 });
+
 export default listItem;
