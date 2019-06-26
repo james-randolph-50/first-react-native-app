22e6b7a9e92c918928b021d99dac6f4e082496c8
Mary Kate <MaryKate@Marys-MBP-3.lan1>
2019-06-26 10:24:35 -0400

    flatlist added

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
 
 import ListItem from '../ListItem/ListItem';
 
 const placeList = props => {
-    const placesOutput = props.places.map((place, i) => (
-        <ListItem key={i} placeName={place} />
-      ));
-
     return (
-        <View style={styles.listContainer}>{placesOutput}</View>
+        <FlatList 
+            style={styles.listContainer}
+            data={props.places}
+            renderItem={() => (
+                <ListItem
+                    placeName={place} 
+                    onItemPressed={() => props.onItemDeleted(i)} 
+                />
+            )}
+        />
     );
 };
 
