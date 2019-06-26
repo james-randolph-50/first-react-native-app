43596783c963e8a618614291842d5bfff5b06e61
Mary Kate <MaryKate@Marys-MBP-3.lan1>
2019-06-26 10:35:36 -0400

    clean up app.js, add image

--- App.js
+++ App.js
@@ -1,37 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, View} from 'react-native';
+import { StyleSheet, View} from 'react-native';
 
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
+import placeImage from './src/assets/'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   state = {
     places: []
-  }
-
+  };
 
   placeAddedHandler = (placeName) => {
     this.setState(prevState => {
       return {
         places: prevState.places.concat({
           key: Math.random(),
-          value: placeName
+          name: placeName,
+          image: placeImage
         })
       };
     });
@@ -40,12 +25,12 @@ export default class App extends Component<Props> {
   placeDeletedHandler = (key) => {
     this.setState(prevState => {
       return {
-        places: prevState.places.filter((place => {
+        places: prevState.places.filter(place => {
           return place.key !== key;
         })
       };
     });
-  }
+  };
 
   render() {
     
